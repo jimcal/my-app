909db89fcd8101fda27fda1d2315301e82d926ff
Jim Liu <jimcal.liu@gmail.com>
2022-11-23 16:02:58 -0800

    Updating next.config.js and hello.js for NextJS Edge Runtime

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = {
+  runtime: 'experimental-edge',
 }
+
+export default async function (req) {
+  return new Response(
+    JSON.stringify({ name: ' Jim Liu'}),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+}
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
